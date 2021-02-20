7a4201e3329982f4254bd960fe1866610815e2f3
강희영 <hekang@hekang-macbook-pro.local>
2021-02-20 22:02:10 +0900

    perfect_square_

--- srcs/square.c
+++ srcs/square.c
@@ -6,20 +6,54 @@
 /*   By: hekang <hekang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 09:59:16 by hekang            #+#    #+#             */
-/*   Updated: 2021/02/19 09:09:19 by hekang           ###   ########.fr       */
+/*   Updated: 2021/02/20 22:01:37 by hekang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-int             vec_each_len(t_vec *a, double l)
+int             vec_each_len(t_vec *a, double l, t_vec *n)
 {
-    if (fabs(a->x) > l)
-        return (FALSE);
-    if (fabs(a->y) > l)
-        return (FALSE);
-    if (fabs(a->z) > l)
-        return (FALSE);
-    return (TRUE);
+    t_vec t;
+    t_vec *t_r;
+    t_vec *tmp;
+
+    // t.x = (n->x) * 180 / PI;
+    // t.y = (n->y) * 180 / PI;
+    // t.z = (n->z) * 180 / PI;
+    
+    // radian = t * 3.141592 / 180;
+    t.x = (n->x) * PI;
+    t.y = (n->y) * PI;
+    t.z = (n->z) * PI;
+    
+    t_r = vec_create(0,0,0);
+    tmp = vec_create(0,0,0);
+
+    //x orient
+    t_r->x = a->x;
+    t_r->y = a->y * cos(t.x) - a->z * sin(t.x);
+    t_r->z = a->y * sin(t.x) + a->z * cos(t.x);
+
+    //z orient
+    tmp->x = t_r->x * cos(t.z) - t_r->y * sin(t.z);
+    tmp->y = t_r->x * sin(t.z) + t_r->y * cos(t.z);
+    tmp->z = t_r->z;
+    //y orient
+    t_r->x = tmp->x * cos(t.y) + tmp->z * sin(t.y);
+    t_r->y = tmp->y;
+    t_r->z = -1 * tmp->x * sin(t.y) + tmp->z * cos(t.y);
+    if ((fabs(t_r->x) < l && fabs(t_r->y) < l && fabs(t_r->z) < l)
+    && (pow(pow(fabs(t_r->x), 2) + pow(fabs(t_r->y), 2) + pow(fabs(t_r->z), 2), 0.5) <= pow(2, 0.5) * l))
+        return (TRUE);
+    
+    return (FALSE);
+}
+
+int             vec_each_len_add(t_vec *a, double l)
+{
+    if (pow(pow(fabs(a->x), 2) + pow(fabs(a->y), 2) + pow(fabs(a->z), 2), 0.5) < pow(2, 0.5) * l / 2)
+        return (TRUE);
+    return (FALSE);
 }
 
 int             square_hit(void *obj, t_ray *r, t_hit_record *rec)
@@ -45,7 +79,10 @@ int             square_hit(void *obj, t_ray *r, t_hit_record *rec)
         중심점에서 각 축(xyz)의 길이의 합이 사이즈 / 2 인 점들.
     */
 
-    if (vec_each_len(vec_sub(rec->p, sq->origin), sq->size / 2))
+    // if (vec_each_len(vec_sub(rec->p, sq->origin), sq->size / 2) 
+    if (vec_each_len(vec_sub(rec->p, sq->origin), sq->size / 2, sq->normal))
+    // && vec_each_len_add(vec_sub(rec->p, sq->origin), sq->size))
+    // if (vec_each_len_add(vec_sub(rec->p, sq->origin), sq->size))
     {
         rec->t = t;
         rec->normal = vec_unit(sq->normal);
